f1d0b3fff1afc6728417fa9b983a4f0649ce8a46
George-Amankwah-Agyenim <georgeagyenimamankwah@gmail.com>
2022-06-01 17:23:29 +0000

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-exceptions = ['q','e']
 for i in range(ord('a'),ord('z') + 1):
-    if chr(i) not in exceptions:
-        print(chr(i), end = '')
+    if chr(i) not in ['q','e']:
+        print(f"{chr(i)}", end = '')
