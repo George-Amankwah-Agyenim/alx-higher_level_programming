fc6bbca03d02a882e6f880064dc4814ae90f3270
George-Amankwah-Agyenim <georgeagyenimamankwah@gmail.com>
2022-06-11 05:32:34 +0000

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    copy = []
-    for i in range(len(my_list)):
+    new_list = my_list[:]
+    for i in range(0, len(my_list)):
         if my_list[i] == search:
-            copy.append(replace)
-        else:
-            copy.append(my_list[i])
+            new_list[i] = replace
     return 
